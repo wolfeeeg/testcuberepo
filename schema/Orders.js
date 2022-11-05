a221209971c4e836af003b44cef5d471f81c3470
2 <2@idfc.ru>
2022-11-05 08:30:49 +0300

    1212

--- schema/Orders.js
+++ schema/Orders.js
@@ -4,6 +4,7 @@ cube(`Orders`, {
   preAggregations: {
     // Pre-Aggregations definitions go here
     // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+    //test11
   },
   
   joins: {
