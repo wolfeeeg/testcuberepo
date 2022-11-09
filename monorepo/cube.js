244b3720f057ac72043dc4c5a67a4cd7fc80849e
Dmitriy L <97888955+wolfeeeg@users.noreply.github.com>
2022-11-09 12:10:54 +0300

    Update cube.js

--- monorepo/cube.js
+++ monorepo/cube.js
@@ -1,1 +1,9 @@
+// NOTE: third-party dependencies and the use of require(...) are disabled for
+// CubeCloud users by default.  Please contact support if you need them
+// enabled for your account.  You are still allowed to require
+// @cubejs-backend/*-driver packages.fdsf22fewfew
+// new test
+// myyy
 
+module.exports = {
+};
