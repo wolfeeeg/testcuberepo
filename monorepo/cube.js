312f7d30e21642bd9ce35dc70e3221bd87897267
system <system@cubecloud.dev>
2022-11-15 11:40:25 +0000

    уц

--- monorepo/cube.js
+++ monorepo/cube.js
@@ -1,13 +1,13 @@
-// NOTE: third-party dependencies and the use of require(...) are disabled for
+/// NOTE: third-party dependencies and the use of require(...) are disabled for
 // CubeCloud users by default.  Please contact support if you need them
 // enabled for your account.  You are still allowed to require
 // @cubejs-backend/*-driver packages.fdsf22fewfew
 // new test
 // myyy fds fds fds f
 //dsadasd
-//tyertsdasfsd fsd fsd
+//tyertsdasfsd fsd fsd 222222
 
-const jwt = require('jsonwebtoken');
+//const jwt = require('jsonwebtoken');
 
 module.exports = {
 };
