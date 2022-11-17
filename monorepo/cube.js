e6e028e0910ac74b1a0ba8f192a57afd545e6ce4
dmitry.leontev <dmitry.leontev@cube.dev>
2022-11-17 07:05:34 +0000

    dfff

--- monorepo/cube.js
+++ monorepo/cube.js
@@ -2,13 +2,10 @@
 // CubeCloud users by default.  Please contact support if you need them
 // enabled for your account.  You are still allowed to require
 // @cubejs-backend/*-driver packages.fdsf22fewfew
-// new test
-// myyy fds fds fds f gdf
-//dsadasd gfdg df g dfs gdfg gdf gdf
 
 const h3 = require("jsonwebtoken");
 
 module.exports = {
 };
 
-//tyertsdasfsd fsd fsd
+//tyertddddsdasfsd fsd fsd
