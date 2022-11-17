c1f987d6f9a66e098f030ccc6d7d923cba069205
Dmitriy L <97888955+wolfeeeg@users.noreply.github.com>
2022-11-17 10:01:44 +0300

    Update cube.js

--- monorepo/cube.js
+++ monorepo/cube.js
@@ -4,7 +4,7 @@
 // @cubejs-backend/*-driver packages.fdsf22fewfew
 // new test
 // myyy fds fds fds f gdf
-//dsadasd gfdg df g dfs gdfg
+//dsadasd gfdg df g dfs gdfg gdf gdf
 
 const h3 = require("jsonwebtoken");
 
