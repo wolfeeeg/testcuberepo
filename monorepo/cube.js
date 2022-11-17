f3b9bf8c77ebbfc67b647090b929a61e3951b6a3
Dmitriy L <97888955+wolfeeeg@users.noreply.github.com>
2022-11-17 12:50:24 +0300

    Update cube.js

--- monorepo/cube.js
+++ monorepo/cube.js
@@ -2,7 +2,7 @@
 // CubeCloud users by default.  Please contact support if you need them
 // enabled for your account.  You are still allowed to require
 // @cubejs-backend/*-driver packages.fdsf22fewfew
-//dddd
+//dddd1111111
 
 const h3 = require("jsonwebtoken");
 
