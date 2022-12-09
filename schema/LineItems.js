aca610fdb5225f5de864742337a3ba09afb2c47f
dmitry.leontev <dmitry.leontev@cube.dev>
2022-12-09 13:55:43 +0000

    111

---
+++ schema/LineItems.js
@@ -0,0 +1,42 @@
+cube(`LineItems`, {
+  sql: `SELECT * FROM public.line_items`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  },
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [id, createdAt]
+    },
+    
+    quantity: {
+      sql: `quantity`,
+      type: `sum`
+    },
+    
+    price: {
+      sql: `price`,
+      type: `sum`
+    }
+  },
+  
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primaryKey: true
+    },
+    
+    createdAt: {
+      sql: `created_at`,
+      type: `time`
+    }
+  }
+});
