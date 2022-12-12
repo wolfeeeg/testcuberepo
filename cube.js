18422189b61f84e059fe030250ce034115192bae
dmitry.leontev <dmitry.leontev@cube.dev>
2022-12-12 19:25:45 +0000

    sdfdsf

--- cube.js
+++ cube.js
@@ -5,7 +5,7 @@
 // enabled for your account.  You are still allowed to require
 // @cubejs-backend/*-driver packages.fdsf22fewfew
 // new test
-// myyy
+// myyy fdsf 
 
 const NodeCache = require('node-cache');
 
